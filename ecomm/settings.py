7446afb718db3031de73b1068a24fe99da01cab2
vinhtuong294 <vinhtuong294@gmail.com>
2025-03-29 23:13:07 +0700

    product detail

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -174,13 +174,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Correctly define STATICFILES_DIRS as a list
+# Update STATICFILES_DIRS to include the static directory
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "public/media"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
